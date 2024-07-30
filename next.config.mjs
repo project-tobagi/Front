cb219d4a16463f41fb4e2b0360fd06d95bee8d12
mecuryPark <qkrghdus1113@naver.com>
2024-07-31 00:36:59 +0900

    * 카카오 지도 수정중(2)

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
         NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
         NAVER_CLIENT_SECRET: process.env.NAVER_CLIENT_SECRET,
         KAKAO_APP_KEY: process.env.KAKAO_APP_KEY,
+        DEPLOY_URL: process.env.DEPLOY_URL,
     },
 };
 
