446c3ff3e6472995c7eec0f7dfffe0be9aa376cc
mecuryPark <qkrghdus1113@naver.com>
2024-10-30 01:04:39 +0900

    - 이슈 개선(4)

--- app/(protected)/components/layout/Layout.tsx
+++ app/(protected)/components/layout/Layout.tsx
@@ -13,7 +13,7 @@ import DividePanel from "../common/divides/DividePanel";
 
 const Layout = ({ children }: any) => {
     return (
-        <div className='min-h-[100vh]'>
+        <div className='h-full overflow-hidden container-none'>
             <ToastContainer pauseOnFocusLoss={false} autoClose={3000} />
             <Header />
 
