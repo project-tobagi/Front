4265417cc4c694ddf77f7111aa0e9e199a9ac8d2
mecuryPark <qkrghdus1113@naver.com>
2024-10-30 00:52:11 +0900

    - 이슈 개선(2)

--- app/(protected)/components/layout/Layout.tsx
+++ app/(protected)/components/layout/Layout.tsx
@@ -17,7 +17,7 @@ const Layout = ({ children }: any) => {
             <ToastContainer pauseOnFocusLoss={false} autoClose={3000} />
             <Header />
 
-            <div className=' p-4 max-lg:p-0'>
+            <div className='p-4 max-lg:p-0'>
                 <DivideGroup>
                     <DividePanel>
                         <Left />
