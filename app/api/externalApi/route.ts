daeba2672ea99e838532de76c5cdf7ee660e4ef4
mecuryPark <qkrghdus1113@naver.com>
2024-11-28 22:01:33 +0900

    이슈개선

--- app/api/externalApi/route.ts
+++ app/api/externalApi/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: any) {
     const { searchParams } = new URL(request.url);
     const dongCode = searchParams.get("dongCode");
     logger.info("polygon api key : ", process.env.POLYGON_API_KEY);
-    const apiUrl = `https://api.vworld.kr/req/data?service=data&version=2.0&request=GetFeature&data=LT_C_ADEMD_INFO&key=${process.env.POLYGON_API_KEY}&domain=http://localhost:3000&attrFilter=emd_cd:=:${dongCode}`;
+    const apiUrl = `https://api.vworld.kr/req/data?service=data&version=2.0&request=GetFeature&data=LT_C_ADEMD_INFO&key=${process.env.POLYGON_API_KEY}&domain=${process.env.API_URL}&attrFilter=emd_cd:=:${dongCode}`;
 
     logger.info(apiUrl);
     try {
