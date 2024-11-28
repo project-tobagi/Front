f72a3397efc653be4f724696ea04455351688fbb
mecuryPark <qkrghdus1113@naver.com>
2024-11-28 21:49:34 +0900

    console.log

--- app/api/externalApi/route.ts
+++ app/api/externalApi/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 export async function GET(request: any) {
     const { searchParams } = new URL(request.url);
     const dongCode = searchParams.get("dongCode");
-
+    console.log("polygon api key : ", process.env.POLYGON_API_KEY);
     const apiUrl = `https://api.vworld.kr/req/data?service=data&version=2.0&request=GetFeature&data=LT_C_ADEMD_INFO&key=${process.env.POLYGON_API_KEY}&domain=http://localhost:3000&attrFilter=emd_cd:=:${dongCode}`;
 
+    console.log(apiUrl);
     try {
         const response = await fetch(apiUrl, {
             headers: {
@@ -13,6 +14,8 @@ export async function GET(request: any) {
                 // 필요한 경우 추가 헤더를 여기에 포함시킵니다
             },
         });
+
+        console.log("response : ", response);
         if (!response.ok) {
             throw new Error("API request failed");
         }
